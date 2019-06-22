765ce772650f0315f087781ee18fd626e6b498bb
Niek Palm <dev.npalm@gmail.com>
2019-06-22 20:49:49 +0200

    Update greeter

--- greeter/app.js
+++ greeter/app.js
@@ -1,35 +1,34 @@
 const express = require('express')
-const request = require('request');
+const request = require('request')
 
 const app = express()
-const HOST = '0.0.0.0';
-const PORT = process.env.PORT || 3000;
-const LOCATION = process.env.LOCATION || 'unknown';
-const TIMEOUT = process.env.TIMEOUT || '3000';
+const HOST = '0.0.0.0'
+const PORT = process.env.PORT || 3000
+const LOCATION = process.env.LOCATION || 'unknown'
+const TIMEOUT = process.env.TIMEOUT || 3000
 
-const WORLD_SERVICE_ENDPOINT = process.env.WORLD_SERVICE_ENDPOINT || 'http://localhost:3000';
-
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const WORLD_SERVICE_ENDPOINT = process.env.WORLD_SERVICE_ENDPOINT || 'http://localhost:3001'
 
 app.get('/', function (req, res) {
-    request(
-        {
-            method: 'GET',
-            url: WORLD_SERVICE_ENDPOINT,
-            timeout: TIMEOUT
-        },
-        
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                res.send('Hello ' + body + 'send via ' + LOCATION)
-            } else {
-                res.send('Oops, nothing received. Send via ' + LOCATION)
-            }      
-    });
+  request(
+    {
+      method: 'GET',
+      url: WORLD_SERVICE_ENDPOINT,
+      timeout: TIMEOUT
+    },
+
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send('Hello ' + body + 'send via ' + LOCATION)
+      } else {
+        console.log(body)
+        console.log(error)
+        res.send('Oops, nothing received. Send via ' + LOCATION)
+      }
+    })
 })
 
-
-app.listen(PORT, HOST);
+app.listen(PORT, HOST)
 
 console.log(`App listening on http://${HOST}:${PORT}!`)
 console.log(`World service endpoint on ${WORLD_SERVICE_ENDPOINT}`)
