1abeddd544fa85be3ba0918728e083644e2af3d4
Niek Palm <dev.npalm@gmail.com>
2019-06-29 22:50:59 +0200

    Dynamic update messages

--- mesh/06-2-deploy-messenger.sh
+++ mesh/06-2-deploy-messenger.sh
@@ -2,4 +2,4 @@ source 00-common.sh
 
 set -x
 export MESSAGE=All good in cluster 2.
-cat demo/messenger/messenger.yaml | sed "s/MESSAGE_TEXt/$MESSAGE/" | kubectl apply -f -
+cat demo/messenger/messenger.yaml | sed "s/MESSAGE_TEXT/$MESSAGE/" | kubectl apply -f -
